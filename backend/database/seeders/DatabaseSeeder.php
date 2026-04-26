42f958c3b8a78dd198f9397a11e7cc7aba2fbcc3
Birovits Bence <birovitsbence@gmail.com>
2026-04-26 21:54:15 +0200

    DatabaseSeeder update

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -49,6 +49,7 @@ class DatabaseSeeder extends Seeder
                     'transmission' => 'Manuális',
                     'engine_size' => 1600,
                     'body_type' => 'Kombi',
+                    'horsepower' => 90,
                 ],
                 'listing' => [
                     'price' => 1,
@@ -63,43 +64,47 @@ class DatabaseSeeder extends Seeder
                     'brand' => 'Volkswagen',
                     'model' => 'Golf 7 GTE',
                     'color' => 'Kék',
-                    'description' => 'Alacsony fogyasztású, megbízható hibrid modell, városi és országúti használatra is.',
+                    'description' => 'A Volkswagen Golf Mk7 GTE a Volkswagen sportos plug-in hibrid modellje. A benzinmotor és villanymotor kombinációja erős gyorsulást és alacsony fogyasztást kínál, miközben megőrzi a Golf kényelmét és mindennapi használhatóságát.',
                     'year' => 2015,
                     'mileage' => 210000,
                     'fuel_type' => 'Hibrid',
                     'transmission' => 'Automata',
                     'engine_size' => 1400,
                     'body_type' => 'Ferdehátú',
+                    'horsepower' => 204,
                 ],
                 'listing' => [
-                    'price' => 1,
+                    'price' => 6767670,
                     'status' => 'active',
                 ],
                 'photos' => [
-
+                    'https://i.imgur.com/5jGNj02.jpeg',
+                    'https://i.imgur.com/IhCbIxn.jpeg',
+                    'https://i.imgur.com/Z421uiX.jpeg',
                 ],
             ],
             [
                 'car' => [
                     'brand' => 'Audi',
-                    'model' => 'A3 2.0 TDI',
+                    'model' => 'A3 8V',
                     'color' => 'Fehér',
-                    'description' => 'Magas felszereltség, kényelmes belső tér, rendszeresen karbantartott állapot.',
+                    'description' => 'Az Audi A3 8V 2.0 CR TDI az Audi kompakt modellje, amely erős és takarékos dízelmotorjáról ismert. Kényelmes, jól összerakott belső térrel és stabil vezethetőséggel ideális mindennapi használatra és hosszabb utakra is.',
                     'year' => 2013,
                     'mileage' => 210000,
                     'fuel_type' => 'Dízel',
                     'transmission' => 'Manuális',
                     'engine_size' => 2000,
                     'body_type' => 'Ferdehátú',
+                    'horsepower' => 150,
                 ],
                 'listing' => [
                     'price' => 6700000,
                     'status' => 'active',
                 ],
                 'photos' => [
-                    'https://i.imgur.com/1zLXiIU.jpeg',
-                    'https://i.imgur.com/IMpbgQY.jpeg',
-                    'https://i.imgur.com/CF5Ss2T.jpeg',
+                    'https://i.imgur.com/WUXyD2s.jpeg',
+                    'https://i.imgur.com/aqcar6E.jpeg',
+                    'https://i.imgur.com/GGVpNod.jpeg',
                 ],
             ],
 
@@ -115,15 +120,16 @@ class DatabaseSeeder extends Seeder
                     'transmission' => 'Manuális',
                     'engine_size' => 1200,
                     'body_type' => 'Sedán',
+                    'horsepower' => 105,
                 ],
                 'listing' => [
                     'price' => 4200000,
                     'status' => 'active',
                 ],
                 'photos' => [
-                    'https://i.imgur.com/JtnJoLj.jpeg',
-                    'https://i.imgur.com/yuaaIMU.jpeg',
-                    'https://i.imgur.com/qFA1afc.jpeg',
+                    'https://i.imgur.com/kw4F2LU.jpeg',
+                    'https://i.imgur.com/l16oKFk.jpeg',
+                    'https://i.imgur.com/EaNxz1m.jpeg',
                 ],
             ],
 
@@ -139,26 +145,253 @@ class DatabaseSeeder extends Seeder
                     'transmission' => 'Manuális',
                     'engine_size' => 2000,
                     'body_type' => 'Ferdehátú',
+                    'horsepower' => 185,
                 ],
                 'listing' => [
                     'price' => 2670000,
                     'status' => 'active',
                 ],
                 'photos' => [
-                    'https://i.imgur.com/YFs9RTR.jpeg',
-                    'https://i.imgur.com/ryyVYO5.jpeg',
-                    'https://i.imgur.com/nJsVS5d.jpeg',
+                    'https://i.imgur.com/5CTeJXC.jpeg',
+                    'https://i.imgur.com/HhZzTVT.jpeg',
+                    'https://i.imgur.com/34nL4yD.jpeg',
+                ],
+            ],
+
+            [
+                'car' => [
+                    'brand' => 'Seat',
+                    'model' => 'Leon FR',
+                    'color' => 'Fehér',
+                    'description' => 'A SEAT Leon Mk1 FR 2.0 TFSI a SEAT első generációs Leonjának sportos változata. A 2.0 TFSI turbós benzinmotor erős és jól tuningolható, miközben a feszes futómű és a sportos megjelenés élvezetes vezetést ad.',
+                    'year' => 2007,
+                    'mileage' => 278000,
+                    'fuel_type' => 'Benzin',
+                    'transmission' => 'Manuális',
+                    'engine_size' => 2000,
+                    'body_type' => 'Ferdehátú',
+                    'horsepower' => 200,
+                ],
+                'listing' => [
+                    'price' => 2670067,
+                    'status' => 'active',
+                ],
+                'photos' => [
+                    'https://i.imgur.com/5h8JYvG.jpeg',
+                    'https://i.imgur.com/bxhib32.jpeg',
+                ],
+            ],
+
+            [
+                'car' => [
+                    'brand' => 'Volkswagen',
+                    'model' => 'Golf GTI',
+                    'color' => 'Fekete',
+                    'description' => 'A Volkswagen Golf Mk6 GTI a Volkswagen ikonikus sportos kompaktja. A 2.0 TSI turbómotor erős és élvezetes vezetést ad, miközben a GTI megőrzi a mindennapi használhatóságot és a klasszikus sportos stílust.',
+                    'year' => 2009,
+                    'mileage' => 230000,
+                    'fuel_type' => 'Benzin',
+                    'transmission' => 'Automata',
+                    'engine_size' => 2000,
+                    'body_type' => 'Ferdehátú',
+                    'horsepower' => 210,
+                ],
+                'listing' => [
+                    'price' => 5555555,
+                    'status' => 'active',
+                ],
+                'photos' => [
+                    'https://i.imgur.com/60qKYSi.jpeg',
+                    'https://i.imgur.com/ekzTTXh.jpeg',
+                    'https://i.imgur.com/ZP0YzPr.jpeg',
+                ],
+            ],
+
+            [
+                'car' => [
+                    'brand' => 'Volvo',
+                    'model' => 'C30 R-Line',
+                    'color' => 'Fehér',
+                    'description' => 'A Volvo C30 T5 a Volvo Cars egyik legizgalmasabb kompakt modellje. Az 5 hengeres turbós benzinmotor erős, jellegzetes hangú és élvezetes vezetést ad, miközben a C30 különleges, sportos formája igazán egyedivé teszi.',
+                    'year' => 2013,
+                    'mileage' => 210000,
+                    'fuel_type' => 'Dízel',
+                    'transmission' => 'Automata',
+                    'engine_size' => 2400,
+                    'body_type' => 'Ferdehátú',
+                    'horsepower' => 184,
+                ],
+                'listing' => [
+                    'price' => 6969690,
+                    'status' => 'active',
+                ],
+                'photos' => [
+                    'https://i.imgur.com/fNISUdM.jpeg',
+                    'https://i.imgur.com/w45paMJ.jpeg',
+                    'https://i.imgur.com/hP9Sioq.jpeg',
+                ],
+            ],
+
+            [
+                'car' => [
+                    'brand' => 'Volkswagen',
+                    'model' => 'Golf 7 R-Line',
+                    'color' => 'Fehér',
+                    'description' => 'A Volkswagen Golf Mk7 1.4 TSI 150 R-Line a Volkswagen egyik népszerű kompaktja. Az 1.4 TSI 150 lóerős turbómotor dinamikus, mégis takarékos, az R-Line csomag pedig sportos külsőt és hangulatot ad a mindennapi használhatóság mellé.',
+                    'year' => 2014,
+                    'mileage' => 174000,
+                    'fuel_type' => 'Benzin',
+                    'transmission' => 'Manuális',
+                    'engine_size' => 1400,
+                    'body_type' => 'Ferdehátú',
+                    'horsepower' => 150,
+                ],
+                'listing' => [
+                    'price' => 4131313,
+                    'status' => 'active',
+                ],
+                'photos' => [
+                    'https://i.imgur.com/5aueTiG.jpeg',
+                    'https://i.imgur.com/rzy3URP.jpeg',
+                    'https://i.imgur.com/ff6Tmhj.jpeg',
+                ],
+            ],
+
+            [
+                'car' => [
+                    'brand' => 'Mercedes',
+                    'model' => 'A 160',
+                    'color' => 'Fekete',
+                    'description' => 'A Mercedes-Benz A160 1.6 Turbo a Mercedes-Benz belépő szintű kompakt modellje. Az 1.6-os turbós benzinmotor kulturált és takarékos, miközben az autó prémium belsőt és kényelmes mindennapi használatot kínál.',
+                    'year' => 2017,
+                    'mileage' => 150000,
+                    'fuel_type' => 'Benzin',
+                    'transmission' => 'Manuális',
+                    'engine_size' => 1600,
+                    'body_type' => 'Ferdehátú',
+                    'horsepower' => 105,
+                ],
+                'listing' => [
+                    'price' => 3131313,
+                    'status' => 'active',
+                ],
+                'photos' => [
+                    'https://i.imgur.com/Oon1nME.jpeg',
+                    'https://i.imgur.com/Rt7sc9U.jpeg',
+                    'https://i.imgur.com/Ws4zVdT.jpeg',
+                ],
+            ],
+
+            [
+                'car' => [
+                    'brand' => 'Audi',
+                    'model' => 'A5',
+                    'color' => 'Szürke',
+                    'description' => 'Az Audi A5 2.0 TFSI az Audi elegáns, sportos kupéja. A 2.0 TFSI turbós benzinmotor jó teljesítményt és kulturált járást kínál, miközben az autó prémium belsővel és kényelmes utazással tűnik ki.',
+                    'year' => 2009,
+                    'mileage' => 200000,
+                    'fuel_type' => 'Benzin',
+                    'transmission' => 'Manuális',
+                    'engine_size' => 2000,
+                    'body_type' => 'Coupe',
+                    'horsepower' => 200,
+                ],
+                'listing' => [
+                    'price' => 13000000,
+                    'status' => 'active',
+                ],
+                'photos' => [
+                    'https://i.imgur.com/GR1CBwK.jpeg',
+                ],
+            ],
+
+            [
+                'car' => [
+                    'brand' => 'BMW',
+                    'model' => 'E36',
+                    'color' => 'Szürke',
+                    'description' => 'A BMW 318tds E36 a BMW 90-es évekbeli 3-as sorozatának takarékos dízel változata. Klasszikus hátsókerék-hajtása miatt így is megmarad a BMW-re jellemző stabil vezethetőség.',
+                    'year' => 2000,
+                    'mileage' => 200000,
+                    'fuel_type' => 'Dízel',
+                    'transmission' => 'Manuális',
+                    'engine_size' => 1700,
+                    'body_type' => 'Sedán',
+                    'horsepower' => 90,
+                ],
+                'listing' => [
+                    'price' => 2130000,
+                    'status' => 'active',
+                ],
+                'photos' => [
+                    'https://i.imgur.com/Pc8HfJW.jpeg',
+                ],
+            ],
+
+            [
+                'car' => [
+                    'brand' => 'Volkswagen',
+                    'model' => 'Golf 6',
+                    'color' => 'Kék',
+                    'description' => 'A Volkswagen Golf Mk6 1.2 TSI a Volkswagen egyik takarékos kompakt modellje. Az 1.2 TSI turbós benzinmotor kis fogyasztás mellett meglepően élénk, így ideális városi és mindennapi használatra.',
+                    'year' => 2012,
+                    'mileage' => 210000,
+                    'fuel_type' => 'Benzin',
+                    'transmission' => 'Manuális',
+                    'engine_size' => 1200,
+                    'body_type' => 'Ferdehátú',
+                    'horsepower' => 105,
+                ],
+                'listing' => [
+                    'price' => 2000000,
+                    'status' => 'active',
+                ],
+                'photos' => [
+                    'https://i.imgur.com/EVAWDb3.jpeg',
+                    'https://i.imgur.com/RXJnhj3.jpeg',
+                    'https://i.imgur.com/gOq2llL.jpeg',
+                    'https://i.imgur.com/mOgbGE4.jpeg',
+                ],
+            ],
+
+            [
+                'car' => [
+                    'brand' => 'Mitsubishi',
+                    'model' => 'Lancer',
+                    'color' => 'Piros',
+                    'description' => 'A Mitsubishi Lancer 1.8 a Mitsubishi Motors megbízható kompakt modellje. Az 1.8-as benzinmotor egyszerű, tartós és mindennapi használatra ideális, miközben a Lancer stabil futóműve kellemes vezethetőséget biztosít.',
+                    'year' => 2013,
+                    'mileage' => 210000,
+                    'fuel_type' => 'Benzin',
+                    'transmission' => 'Automata',
+                    'engine_size' => 1800,
+                    'body_type' => 'Sedán',
+                    'horsepower' => 145,
+                ],
+                'listing' => [
+                    'price' => 3333333,
+                    'status' => 'active',
+                ],
+                'photos' => [
+                    'https://i.imgur.com/jI4uEhF.png',
+                    'https://i.imgur.com/ElJKoiD.png',
                 ],
             ],
         ];
 
         foreach ($examples as $index => $example) {
-            $car = Car::create($example['car']);
+            $carData = $example['car'];
+            $horsepower = (int) ($carData['horsepower'] ?? $example['listing']['horsepower'] ?? 0);
+
+            unset($carData['horsepower']);
+
+            $car = Car::create($carData);
 
             $listing = Listings::create([
                 'user_id' => $user->id,
                 'car_id' => $car->id,
                 'price' => $example['listing']['price'],
+                'horsepower' => $horsepower,
                 'status' => $example['listing']['status'],
             ]);
 
